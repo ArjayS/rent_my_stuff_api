16889b50796679db36f0858fe5fc8ad6592d9622
Robert Servado <servadorobertgmail.com>
2022-11-18 13:57:31 -0500

    Modified server.js to include middleware

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
+
+//middleware
+app.use(morgan("dev"));
+app.use(express.json);
+
+// routes
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
