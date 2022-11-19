2bdf34c37a226172298e7cce87643cd7cdd10977
Robert Servado <servadorobertgmail.com>
2022-11-19 16:17:53 -0500

    Modified server.js to add baic route connections

--- server.js
+++ server.js
@@ -1,14 +1,75 @@
 require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan");
+const db = require("./db");
 
 const app = express();
+const morgan = require("morgan");
 
-//middleware
+// //middleware
 app.use(morgan("dev"));
-app.use(express.json);
+app.use(express.json());
 
-// routes
+// // routes
+
+// Get all items
+app.get("/api/items", async (req, res) => {
+  const results = await db.query("SELECT * FROM items");
+  console.log(results);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      items: ["stapler", "pen"],
+    },
+  });
+});
+
+// Get an item
+app.get("/api/item/:id", (req, res) => {
+  console.log(req.params);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      items: "stapler",
+    },
+  });
+});
+
+// Create an item
+app.post("/api/items", (req, res) => {
+  console.log(req.body);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      items: "stapler",
+    },
+  });
+});
+
+// Update an item
+app.put("/api/item/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      items: "scissors",
+    },
+  });
+});
+
+// Delete an item
+app.delete("/api/item/:id", (req, res) => {
+  res.status(204).json({
+    status: "success",
+  });
+});
+
+// // /api/units
+// // /api/users
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
