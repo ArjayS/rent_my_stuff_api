59a01076642358493c4956ea634e3429c3f3f601
Robert Servado <servadorobertgmail.com>
2022-11-23 15:58:12 -0500

    Added router/UserReviews.js for USerReviews router

---
+++ router/UserReviews.js
@@ -0,0 +1,49 @@
+module.exports = function (router, db) {
+  // Getting a user review for a renter
+  router.get("/renter/:id", async (req, res) => {
+    try {
+      const results = await db.query(
+        "SELECT * FROM user_reviews LEFT JOIN (SELECT id, user_name, user_image FROM users GROUP BY id) users ON users.id = user_reviews.owner_id RIGHT JOIN (SELECT id, item_name, item_base_price, item_image, item_id, rsrv_price_bid FROM items LEFT JOIN (SELECT item_id, rsrv_price_bid FROM reservations) reservations ON items.id = reservations.item_id) items ON items.id = user_reviews.item_id WHERE guest_id = $1",
+        [req.params.id]
+      );
+
+      console.log(results);
+
+      res.status(200).json({
+        status: "Successfully got the user reviews!",
+        results: results.rows.length,
+        data: {
+          userReview: results.rows,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  router.post("/renter/:id", async (req, res) => {
+    try {
+      const results = await db.query(
+        "INSERT INTO user_reviews (guest_id, owner_id, item_id, rent_worthy, rent_message) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+        [
+          req.params.id,
+          req.body.owner_id,
+          req.body.item_id,
+          req.body.rent_worthy,
+          req.body.rent_message,
+        ]
+      );
+
+      console.log(results);
+
+      res.status(200).json({
+        status: "Successfully created a renter review!",
+        data: {
+          user: results.rows[0],
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
