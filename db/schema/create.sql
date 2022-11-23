cd7aae6836cb0cd38190cf3f4b5e886c7d6b98c2
Robert Servado <servadorobertgmail.com>
2022-11-23 08:20:03 -0500

    Modified db/schema/create.sql to be more detail

--- db/schema/create.sql
+++ db/schema/create.sql
@@ -4,33 +4,36 @@ DROP TABLE IF EXISTS reservations CASCADE;
 DROP TABLE IF EXISTS user_reviews CASCADE;
 DROP TABLE IF EXISTS item_reviews CASCADE;
 
+CREATE DATABASE rentmystuff;
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
+  user_name VARCHAR(255) NOT NULL,
+  user_email VARCHAR(255) NOT NULL,
+  user_password VARCHAR(255) NOT NULL,
   user_image VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE items (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  title VARCHAR(255) NOT NULL,
-  location VARCHAR(255) NOT NULL,
-  status BOOLEAN NOT NULL DEFAULT TRUE,
+  item_name VARCHAR(255) NOT NULL,
+  item_location VARCHAR(255) NOT NULL,
+  item_base_price INTEGER NOT NULL DEFAULT 0,
+  item_status BOOLEAN NOT NULL DEFAULT TRUE,
   item_image VARCHAR(255) NOT NULL,
-  description TEXT
+  item_description TEXT
 );
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   item_id INTEGER REFERENCES items(id) ON DELETE CASCADE NOT NULL,
-  start_date DATE,
-  end_date DATE,
-  date_returned DATE,
-  item_price INTEGER NOT NULL DEFAULT 0,
-  approval BOOLEAN NOT NULL DEFAULT FALSE
+  rsrv_start_date DATE,
+  rsrv_end_date DATE,
+  rsrv_date_returned DATE,
+  rsrv_price_bid INTEGER NOT NULL DEFAULT 0,
+  rsrv_approval BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE user_reviews (
@@ -38,14 +41,14 @@ CREATE TABLE user_reviews (
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   item_id INTEGER REFERENCES items(id) ON DELETE CASCADE,
-  trustworthy INTEGER NOT NULL check(trustworthy >= 1 AND trustworthy <= 10),
-  message text
+  rent_worthy INTEGER NOT NULL check(rent_worthy >= 1 AND rent_worthy <= 10),
+  rent_message TEXT
 );
 
 CREATE TABLE item_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   item_id INTEGER REFERENCES items(id) ON DELETE CASCADE,
-  rating INTEGER NOT NULL check(rating >= 1 AND rating <= 5),
-  message text
+  item_rating INTEGER NOT NULL check(item_rating >= 1 AND item_rating <= 5),
+  item_message TEXT
 );
