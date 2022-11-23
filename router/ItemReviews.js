de4ba5c9e6d257eec7b3c9116f1d879725dc5c63
Robert Servado <servadorobertgmail.com>
2022-11-23 16:35:19 -0500

    Modified router/ItemReviews.js to include seeing all reviews and creating a new item review

--- router/ItemReviews.js
+++ router/ItemReviews.js
@@ -1,1 +1,51 @@
-module.exports = function (router, db) {};
+module.exports = function (router, db) {
+  // Getting an item review
+  router.get("/stuff/:id", async (req, res) => {
+    try {
+      const results = await db.query(
+        "SELECT * FROM item_reviews LEFT JOIN (SELECT id, user_name, user_image FROM users) users ON users.id = item_reviews.guest_id WHERE item_id = $1",
+        [req.params.id]
+      );
+
+      console.log(results);
+
+      res.status(200).json({
+        status: "Successfully got the user reviews!",
+        results: results.rows.length,
+        data: {
+          itemReviews: results.rows,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  // Creating a new item review based on item
+  router.post("/stuff/:id", async (req, res) => {
+    try {
+      const results = await db.query(
+        "INSERT INTO item_reviews (guest_id, item_id, item_rating, item_message) VALUES ($1, $2, $3, $4) RETURNING *",
+        [
+          req.body.guest_id,
+          req.params.id,
+          req.body.item_rating,
+          req.body.item_message,
+        ]
+      );
+
+      console.log(results);
+
+      res.status(200).json({
+        status: "Successfully created an item review!",
+        data: {
+          user: results.rows[0],
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  return router;
+};
